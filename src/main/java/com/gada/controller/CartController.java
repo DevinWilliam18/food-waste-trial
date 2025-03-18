27767edfc295b3b1ca3973a73e28fe81f3f65540
Devins11 <devinfcb42@gmail.com>
2025-03-18 23:58:07 +0700

    added controller classes

---
+++ src/main/java/com/gada/controller/CartController.java
@@ -0,0 +1,8 @@
+package com.gada.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/controller")
+public class CartController {
+
+}
---
+++ src/main/java/com/gada/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.gada.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/product")
+public class ProductController {
+
+
+
+}
