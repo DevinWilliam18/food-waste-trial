d5dce072a8b2dac55c4adcc6472b9a63b4fcf7ea
Devins11 <devinfcb42@gmail.com>
2025-03-21 23:55:42 +0700

    added controller classes - Product

--- src/main/java/com/gada/controller/ProductController.java
+++ src/main/java/com/gada/controller/ProductController.java
@@ -1,11 +1,23 @@
 package com.gada.controller;
 
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/product")
 public class ProductController {
 
+    @PostMapping("/add")
+    private String addView(@RequestParam){
 
+    }
+
+    @GetMapping("/")
+    private String showPage(Model model){
+
+    }
 
 }
