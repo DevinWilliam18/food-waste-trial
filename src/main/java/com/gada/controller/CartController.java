64e9edfbed6f07c1985a38c251829a5e4bfb86b1
Devins11 <devinfcb42@gmail.com>
2025-03-29 23:56:56 +0700

    updated controller

--- src/main/java/com/gada/controller/CartController.java
+++ src/main/java/com/gada/controller/CartController.java
@@ -1,8 +1,13 @@
 package com.gada.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/cart")
 public class CartController {
+
+    @PostMapping("/add")
+    private void S
+
 
 }
--- src/main/java/com/gada/controller/ProductController.java
+++ src/main/java/com/gada/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.gada.controller;
 
 
+import com.gada.model.Product;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/product")
 public class ProductController {
@@ -17,8 +21,22 @@ public class ProductController {
 
     @GetMapping("/show")
     private String showPage(Model model){
+        model.addAttribute();
+
+        return "";
+    }
+
+    @GetMapping("/search")
+    private String search(){
 
     }
+
+    private List<Product> getAllFoods(){
+        List<Product> foods = new ArrayList<>();
+
+        return foods;
+    }
+
 
 
 }
