634f507929a6a4eb9755df349da404e66dcd7886
Devins11 <devinfcb42@gmail.com>
2025-03-31 23:56:01 +0700

    modified controller class

--- src/main/java/com/gada/controller/CartController.java
+++ src/main/java/com/gada/controller/CartController.java
@@ -1,13 +1,90 @@
 package com.gada.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.gada.model.Cart;
+import com.gada.service.CartService;
+import com.gada.service.impl.CartServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/cart")
 public class CartController {
 
-    @PostMapping("/add")
-    private void S
+    private String CART_MAIN_PAGE = "templates/buyer/cart_page.html";
 
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/add")
+    private ResponseEntity<Map<String, String>> add(@RequestParam String id, HttpServletRequest request){
+
+        Map<String, String> response = new HashMap<>();
+        try{
+            cartService.addFoodToCart(String id, request);
+            //call service class
+
+
+
+
+        }catch(Exception e){
+            response.put("status", "success");
+            response.put("code", "");
+            response.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        response.put("status", "success");
+        response.put("code", "");
+        response.put("message", "");
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/remove-cart")
+    private ResponseEntity<Map<String, String>> remove(@RequestParam String id){
+        // check if the item still exists
+
+        //delete service
+        Map<String, String> response = new HashMap<>();
+        try{
+
+        }catch(Exception e){
+            response.put("", "");
+            response.put("", "");
+            response.put("", "");
+
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        response.put("", "");
+        response.put("", "");
+        response.put("", "");
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/show")
+    private String show (Model model){
+
+        List<Cart> cartList = getAllCarts();
+
+        model.addAttribute("items",cartList);
+
+        return CART_MAIN_PAGE;
+    }
+
+    private List<Cart> getAllCarts(){
+        List<Cart> cartList = new ArrayList<>();
+
+        return cartList;
+    }
 
 }
