b0e1cc3dbb18f175329e11759ebc18eed3de5fcd
Devins11 <devinfcb42@gmail.com>
2025-04-06 23:55:58 +0700

    updated sidebar menu

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -4,20 +4,35 @@ $(document).ready(function() {
     // Open sidebar when clicking the icon
     $('#list-menu').click(function(e) {
         console.log("masuk click")
+        $(".sidebar").toggleClass("collapsed");
         e.preventDefault(); // Prevent default anchor behavior
-        $('#sidebarPopup').addClass('open');
-        $('#overlay').show();
+        console.log("after sidebar..!!")
+        $('.sidebarPopup').addClass('open');
+        // $('#overlay').show();
+        // Push or reset main content
+        if ($(".sidebar").hasClass("collapsed")) {
+            console.log("if")
+            $("#main").css("margin-left", "0"); // Sidebar closed
+        } else {
+            console.log("else")
+            $("#main").css("margin-left", "264px"); // Sidebar open
+        }
     });
 
-    // Close sidebar when clicking the close button
+    // Close sidebar and reset main content
     $('#closeSidebar').click(function() {
+        $("#sidebar").addClass("collapsed");
         $('#sidebarPopup').removeClass('open');
         $('#overlay').hide();
+        $("#main").css("margin-left", "0");
     });
 
-    // Close sidebar when clicking the overlay
+    // Close sidebar via overlay and reset main content
     $('#overlay').click(function() {
+        $("#sidebar").addClass("collapsed");
         $('#sidebarPopup').removeClass('open');
         $('#overlay').hide();
+        $("#main").css("margin-left", "0");
     });
+
 });
