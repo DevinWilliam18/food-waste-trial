b24cf3145cac4702242894b06529296265734c15
Devins11 <devinfcb42@gmail.com>
2025-04-10 23:52:25 +0700

    Updated the dao interface

--- src/main/java/com/gada/dao/MenuDao.java
+++ src/main/java/com/gada/dao/MenuDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MenuDao extends JpaRepository<Menu, String> {
+public interface MenuDao extends JpaRepository<Menu, String> {
 }
