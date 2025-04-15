ab8423b4d2a2875e8edd7e3aa1cf688e45bc346d
Devins11 <devinfcb42@gmail.com>
2025-04-15 23:58:29 +0700

    Added postgres driver

--- src/main/java/com/gada/dao/CartDao.java
+++ src/main/java/com/gada/dao/CartDao.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CartDao extends JpaRepository<Cart, String> {
+public interface CartDao extends JpaRepository<Cart, String> {
 
 }
