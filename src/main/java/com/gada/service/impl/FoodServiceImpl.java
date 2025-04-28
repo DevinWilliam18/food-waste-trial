12d8388b59f42911dcb1eff61d1406a9ea6adda0
Devins11 <devinfcb42@gmail.com>
2025-04-29 05:32:29 +0700

    Updated food service

--- src/main/java/com/gada/service/impl/FoodServiceImpl.java
+++ src/main/java/com/gada/service/impl/FoodServiceImpl.java
@@ -1,8 +1,15 @@
 package com.gada.service.impl;
 
+import com.gada.model.Product;
 import com.gada.service.FoodService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class FoodServiceImpl extends FoodService {
+public class FoodServiceImpl implements FoodService {
+    @Override
+    public List<Product> get() {
+        return null;
+    }
 }
